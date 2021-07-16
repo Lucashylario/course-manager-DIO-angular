a6f662f39fa076de25abc401864e3a1a92c7475f
Lucashylario <lucashyfrotend@hotmail.com>
2021-07-16 07:51:42 -0300

    injeção de dependência p/ course

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,6 +1,7 @@
 import { stringify } from "@angular/compiler/src/util";
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     selector: 'app-course-list',
@@ -11,28 +12,9 @@ export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) { }
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageURL: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,  
-                releaseDate: 'November, 2, 2021'                                                     
-            },
-            {
-                id: 1,
-                name: 'Angular: HTTP',
-                imageURL: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 88,
-                rating: 4,    
-                releaseDate: 'December, 2, 2021'                                                   
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
+
 }
